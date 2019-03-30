532ea49c157189bd689bda0b20897fb247b21d18
Brian Christensen <brianchristensen@outlook.com>
2019-03-30 10:58:42 -0700

    refactoring

--- text_client/lib/text_client/interact.ex
+++ text_client/lib/text_client/interact.ex
@@ -1,10 +1,22 @@
 defmodule TextClient.Interact do
+  @hangman_server :"hangman@brian-mbp"
+
   alias TextClient.{State, Player}
 
   def start() do
-    Hangman.new_game()
+    new_game()
     |> setup_state()
     |> Player.play()
+  end
+
+  defp new_game() do
+    Node.connect(@hangman_server)
+    :rpc.call(
+      @hangman_server,
+      Hangman,
+      :new_game,
+      []
+    )
   end
 
   defp setup_state(game_pid) do
