a993810e567940c8cdd27144b5d53f830131eb7b
Brian Christensen <brianchristensen@outlook.com>
2019-04-01 15:06:58 -0700

    hangman web ui working

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,3 +1,34 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+
+  @responses %{
+    :won => { :success, "You Won!" },
+    :lost => { :danger, "You Lost!" },
+    :good_guess => { :success, "Good guess!" },
+    :bad_guess => { :warning, "Bad guess!" },
+    :already_used => { :info, "You already guessed that" }
+  }
+
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [:won, :lost]
+  end
+
+  def new_game_button(conn) do
+    button("New Game", to: Routes.hangman_path(conn, :create_game), autofocus: "autofocus")
+  end
+
+  def game_state(state) do
+    @responses[state]
+    |> alert()
+  end
+
+  defp alert(nil), do: ""
+  defp alert({class, msg}) do
+    """
+    <div class="alert alert-#{class}">
+      #{msg}
+    </div>
+    """
+    |> raw()
+  end
 end
